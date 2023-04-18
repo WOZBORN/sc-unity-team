36385f6cc8cab0be23f6eca5445da5c5e27cf0d4
Horpikc <128626564+Horpikc@users.noreply.github.com>
2023-04-18 17:43:25 +0300

    normal shooting

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,26 +1,55 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shoot : MonoBehaviour
 {
     public float damage = 20f;
-    public float range = 100f;
+    public float range = 150f;
+    public float timer = 5;
+    public float magaz = 30;
     public AudioSource shotSound;
 
     public Camera fpsCam;
+    public ParticleSystem muzzleflash;
+    public Text magaztext;
+
+    void Start()
+    {
+        magaztext.text = magaz.ToString();
+    }
 
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            shotSound.PlayOneShot(shotSound.clip);
-
-            Shoots();
+            if (magaz >= 1)
+            {
+                timer += 2 * Time.deltaTime;
+                if (timer >= 0.5f)
+                {
+                    Shoots();
+                    magaz -= 1;
+                    timer = 0;
+                }
+            }
         }
+        if (magaz == 0)
+        {
+            if (timer >= 6)
+            {
+                magaz = 30;
+            }
+            timer += 2 * Time.deltaTime;
+        }
+        magaztext.text = Mathf.Round(magaz).ToString();
     }
     void Shoots()
     {
+        muzzleflash.Play();
+        shotSound.PlayOneShot(shotSound.clip);
         RaycastHit hit;
+
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
